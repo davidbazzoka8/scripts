0b2e9834f1876ba460da77b4d4acb44fad88b7e0
davidbazzoka8 <davidbazzoka8+yy@gmail.com>
2025-04-18 10:39:05 +0800

    Create main.lua

---
+++ .github/workflows/main.lua
@@ -0,0 +1,6 @@
+loadstring(game:HttpGet("https://yarhm.mhi.im/scr", false))() -- thanks mochig for domain & hosting
+
+-- Optional loading from GitHub if the website is down
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua"))(
+
+)
